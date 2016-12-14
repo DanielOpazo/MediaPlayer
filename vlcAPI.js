57d4ee4492466440378e68b55f4a8e34f9ad5415
dopazo <daniel.opazo.baer@gmail.com>
2016-12-13 23:51:09 -0500

    added function to call vlc api using http

--- vlcAPI.js
+++ vlcAPI.js
@@ -1,5 +1,5 @@
 /*
-* created by Daniel Opazo, 2016/12/12
+ * created by Daniel Opazo, 2016/12/12
 */
 
 /* child_process will be used to start the VLC process */
@@ -11,7 +11,8 @@ var http = require('http');
 options = {
 	player: '/usr/bin/cvlc',
 	communication: 'http',
-	port : '8080'
+	port : '8080',
+	password: 'password'
 };
 
 /* puts the arguments to vlc in proper format */
@@ -32,6 +33,11 @@ if (options.port !== undefined) {
 	pargs.push(options.port);
 }
 
+if (options.password !== undefined) {
+	pargs.push('--http-password');
+	pargs.push(options.password);
+}
+
 /* start the vlc process */
 var player = child_process.spawn(options.player, pargs);
 
@@ -44,3 +50,41 @@ player.stderr.on('data', function(data) {
 player.stdout.on('data', function(data) {
 	console.log('ERROR ' + data);
 });
+
+
+/* Call the vlc api
+*/
+
+function apiCall(options, command, args, callback) {
+	var urlArgs = '';
+
+	/* put the arguments into the proper form for the http get */
+	for (var key in args) {
+		urlArgs = '&' + encodeURIComponent(key) + '=' + encodeURIComponent(args[key]);
+	}
+
+	if (command !== undefined) {
+		command = '?command=' + command;
+	}else { //this should be handled better
+		console.log('ERROR command is undefined');
+		return;
+	}
+	
+	/* call the vlc api using http */
+	http.get({
+		hostname: 'localhost',
+		port: options.port,
+		path: '/requests/status.json' + commands + urlArgs,
+		auth: ':' + options.password,
+		agent: false
+	}, function (res) {
+		if (callback !== undefined) {
+			callback(res);
+		}
+	});	
+}
+
+
+
+
+
