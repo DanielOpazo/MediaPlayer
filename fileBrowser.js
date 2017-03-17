411c89c755b6948d6016723bdf9255cd07983397
dopazo <daniel.opazo.baer@gmail.com>
2017-03-16 21:20:22 -0400

    change filebrowser to use new keys in JSON. One schema for directories, one for files. Also read file metadata, and add to file JSON

--- fileBrowser.js
+++ fileBrowser.js
@@ -1,15 +1,58 @@
 var fs = require("fs");
 var path = require("path");
+var ffmpeg = require('fluent-ffmpeg');
 
-/* The current directory listing. Each entry in the array
- * has a key of a string file or directory name, and a value
- * of type fileTypeEnum, that indicates if the entry is a file
- * or a directory
+/*
+ * Schema definitions
+ * Schema for folder objects:
+ * "folders": [
+    {
+      "name":"<string>",
+      "numItems":<int>
+    }
 */
-var fileList = {};
-var fileTypeEnum = {
-    TYPE_FILE : 1,
-    TYPE_DIR : 2
+var FOLDERS_KEY = "folders";
+var FOLDERS_DIR_KEY = "name";
+var FOLDERS_DIR_NUM_ITEMS = "numItems"
+
+/*
+ * Schema for video objects
+ * videos": [
+ *  {
+ *     "name":"<string>",
+ *     "duration":<int>
+ *  },
+*/
+var VIDEOS_KEY = "videos";
+var VIDEOS_TITLE_KEY = "name";
+var VIDEOS_LENGTH_KEY = "duration";
+
+/*
+ * Global list definitions
+*/
+var fileList = [];
+var directoryList = [];
+
+/*
+ * Read metadata using ffmpeg
+ * @param filePath (string) path to file to be queried
+ * @param cb (function) callback function that is passed the metadata object
+*/
+function getMetaData(filePath, cb) {
+    ffmpeg.ffprobe(filePath, function(err, metadata) {
+        cb(metadata);
+    });
+}
+
+/*
+ * get file duration from ffmpeg metadata object
+ * @param filePath (string) path to file to be queried
+ * @param cb (function) callback function that is passed the file length (in seconds)
+*/
+function getMediaFileDuration(filePath, cb) {
+    getMetaData(filePath, function(metadata) {
+        cb(metadata["format"]["duration"]);
+    });
 }
 
 //checks the file extension against the list of allowed filetypes
@@ -18,8 +61,39 @@ function validFile(file) {
     return extensionPatt.test(file);
 }
 
-function resetFileList () {
-    fileList = {};
+function resetLists () {
+    fileList = [];
+    directoryList = [];
+}
+
+/*
+ * Get number of items in a directory.
+ * This is a dumb length, in the sense that it doesn't do any
+ * validation on what the items in the directory are. 
+ * @param dirPath (string) relative path of directory to browse
+ * @param cb (function) callback function to process result
+*/
+function getNumItemsInDir(dirPath, cb) {
+    fs.readdir(dirPath, function (err, files) {
+        if (err)
+            cb(err);
+        else
+            cb(files.length);
+    });
+}
+
+function addFileToList(pathToFile, fileName, cb) {
+    if (validFile(pathToFile)) {
+        getMediaFileDuration(pathToFile, function(duration) {
+            var fileItem = {};
+            fileItem[VIDEOS_TITLE_KEY] = fileName;
+            fileItem[VIDEOS_LENGTH_KEY] = duration;
+            fileList.push(fileItem);
+            cb();
+        });
+    }else {
+        cb();
+    }
 }
 
 /* depth first recursive file list
@@ -39,14 +113,14 @@ function parseDir(dir, goDeeper, cb) {
  * handles files/directories one at a time, then moves to the
  * next one by invoking itself again.
  * If the file/directory passes validation, it is added to the global
- * variable array fileList
+ * variable array fileList, or directoryList
  * @param dir (string) The directory to browse
  * @param files (array) The list of files/directories in the directory
  * @param goDeeper (boolean) Controls whether the browsing should go into 
  *                           subdirectories or not. If goDeeper is true,
                              every subdirectory will be browsed, and fileList
                              will contain the entire file tree, starting from dir.
- * @paramn cb (function) callback function. Lets the caller decide what to do once
+ * @param cb (function) callback function. Lets the caller decide what to do once
                          the directory has been browsed, and fileList is filled 
  */
 function handleFiles(dir, files, goDeeper, cb) {
@@ -67,15 +141,26 @@ function handleFiles(dir, files, goDeeper, cb) {
                                 handleFiles(dir, files, goDeeper, cb);
                         });
                     }else {
-                        //add the directory to the fileList, then move on without going into the directory
-                        fileList[p] = fileTypeEnum.TYPE_DIR;
+                        //add the directory to the directoryList, then move on without going into the directory
+                        //skip hidden folders
+                        if (file[0] != '.') {
+                            getNumItemsInDir(p, function(numItems) {
+                                //no full paths for now
+                                //if I want to use variables for my keys, I have to do this stupid
+                                //multiline assignment to the object
+                                var dirItem = {};
+                                dirItem[FOLDERS_DIR_KEY] = file;
+                                dirItem[FOLDERS_DIR_NUM_ITEMS] = numItems;
+                                directoryList.push(dirItem);
+                            });
+                        }
                         handleFiles(dir, files, goDeeper, cb);
                     }
                 }else if (stats.isFile()) {
-                    if (validFile(p))
-                        fileList[p] = fileTypeEnum.TYPE_FILE;
-                    //move on to the next file
-                    handleFiles(dir, files, goDeeper, cb);
+                    addFileToList(p, file, function() {
+                        //move on to the next file
+                        handleFiles(dir, files, goDeeper, cb);
+                    });
                 }
             }
         });
@@ -91,12 +176,15 @@ function handleFiles(dir, files, goDeeper, cb) {
 var exports = module.exports = function() {
     return {
         browse: function (dir, cb) {
-                    resetFileList();
+                    resetLists();
                     parseDir(dir, false, function (err) {
                         if (err)
-                            console.error(fileList);
+                            console.error("Error parsing directory: " + dir);
                         else {
-                            cb(JSON.stringify(fileList));
+                            var dirInfo = {};
+                            dirInfo[FOLDERS_KEY] = directoryList;
+                            dirInfo[VIDEOS_KEY] = fileList;
+                            cb(dirInfo);
                         }
                     });
                 }
