9cbf073cf708aed6d5917e7328d4ee212c6fecf5
dopazo <daniel.opazo.baer@gmail.com>
2017-03-20 23:41:47 -0400

    Added logging of client ip addresses

--- appAPI.js
+++ appAPI.js
@@ -8,7 +8,7 @@ var fileBrowser = require('./fileBrowser.js');
 var fb = new fileBrowser();
 
 app.get('/command/play', function (req, res) {
-    console.log("received play command with url parameter media =" + req.query.media);
+    console.log("received play command from " + req.ip + " with url parameter media =" + req.query.media);
     player.play(req.query.media, function () {
         console.log("play command sent");
     });
@@ -16,20 +16,25 @@ app.get('/command/play', function (req, res) {
 });
 
 app.get('/command/pause', function (req, res) {
-    console.log("received pause command");
+    console.log("received pause command from " + req.ip);
     res.send();
 });
 
 app.get('/command/stop', function (req, res) {
-    console.log("received stop command");
+    console.log("received stop command from " + req.ip);
     player.stop(function () {
         console.log("stop command sent");
     });
     res.send();
 });
 
+app.get('/command/discover', function (req, res) {
+    console.log("received discover command from " + req.ip);
+    res.send("Daniel Media Player");
+});
+
 app.get('/browse', function (req, res) {
-    console.log("received browse command with url parameter dir = " + req.query.dir);
+    console.log("received browse command from " + req.ip + " with url parameter dir = " + req.query.dir);
     //should probably validate the path somehow, It should be validated at the fileBrowser.js level though
     fb.browse(req.query.dir, function (retVal) {
         console.log(retVal);
