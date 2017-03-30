b27e47cf0c75a91051c8563c7554b1a1db6da7f3
dopazo <daniel.opazo.baer@gmail.com>
2017-03-30 01:00:06 -0400

    reordered alphabetically express routing

--- app/appAPI.js
+++ app/appAPI.js
@@ -31,8 +31,54 @@ function getIPv4Address() {
     return ipv4address;
 }
 
+app.get('/browse', function (req, res) {
+    console.log("received browse command from " + req.ip + " with url parameter dir = " + req.query.dir);
+    //should probably validate the path somehow, It should be validated at the fileBrowser.js level though
+    fb.browse(req.query.dir, function (retVal) {
+        console.log(retVal);
+        res.json(retVal);
+    });
+});
+
+app.get('/command/close', function (req, res) {
+    console.log("received close command from " + req.ip);
+    player.close();
+    console.log("close command sent");
+    res.send();
+});
+
+app.get('/command/discover', function (req, res) {
+    console.log("received discover command from " + req.ip);
+    var obj = {};
+    res.json({name: os.hostname(), home: os.homedir(), address: getIPv4Address()});
+});
+
+app.get('/command/emptyPlaylist', function (req, res) {
+    console.log("received emptyPlaylist command from " + req.ip);
+    player.emptyPlaylist(function (vlcRes) {
+        console.log("emptyPlaylist command sent");
+    });
+    res.send();
+});
+
+app.get('/command/fullscreen', function (req, res) {
+    console.log("received fullscreen command from " + req.ip);
+    player.fullscreen(function (vlcRes) {
+        console.log("fullscreen command sent");
+    });
+    res.send();
+});
+
 app.get('/command/interfaces', function (req, res) {
     getIPv4Address();
+    res.send();
+});
+
+app.get('/command/pause', function (req, res) {
+    console.log("received pause command from " + req.ip);
+    player.pause(function(vlcRes) {
+        console.log("pause command sent");
+    });
     res.send();
 });
 
@@ -52,12 +98,12 @@ app.get('/command/seek', function (req, res) {
     res.send();
 });
 
-app.get('/command/pause', function (req, res) {
-    console.log("received pause command from " + req.ip);
-    player.pause(function(vlcRes) {
-        console.log("pause command sent");
+app.get('/command/status', function (req, res) {
+    console.log("received status command from " + req.ip);
+    player.status(function (status) {
+        console.log("status command sent");
+        res.send(status)
     });
-    res.send();
 });
 
 app.get('/command/stop', function (req, res) {
@@ -68,58 +114,12 @@ app.get('/command/stop', function (req, res) {
     res.send();
 });
 
-app.get('/command/close', function (req, res) {
-    console.log("received close command from " + req.ip);
-    player.close();
-    console.log("close command sent");
-    res.send();
-});
-
-app.get('/command/discover', function (req, res) {
-    console.log("received discover command from " + req.ip);
-    var obj = {};
-    res.json({name: os.hostname(), home: os.homedir(), address: getIPv4Address()});
-});
-
-app.get('/command/fullscreen', function (req, res) {
-    console.log("received fullscreen command from " + req.ip);
-    player.fullscreen(function (vlcRes) {
-        console.log("fullscreen command sent");
-    });
-    res.send();
-});
-
-app.get('/command/emptyPlaylist', function (req, res) {
-    console.log("received emptyPlaylist command from " + req.ip);
-    player.emptyPlaylist(function (vlcRes) {
-        console.log("emptyPlaylist command sent");
-    });
-    res.send();
-});
-
-app.get('/command/status', function (req, res) {
-    console.log("received status command from " + req.ip);
-    player.status(function (status) {
-        console.log("status command sent");
-        res.send(status)
-    });
-});
-
 app.get('/command/volume', function (req, res) {//this needs validation to not set volume above 512
     console.log("received volume command from " + req.ip);
     player.volume(req.query.val, function (vlcRes) {
         console.log("volume command sent");
     });
     res.send();
-});
-
-app.get('/browse', function (req, res) {
-    console.log("received browse command from " + req.ip + " with url parameter dir = " + req.query.dir);
-    //should probably validate the path somehow, It should be validated at the fileBrowser.js level though
-    fb.browse(req.query.dir, function (retVal) {
-        console.log(retVal);
-        res.json(retVal);
-    });
 });
 
 function startServer(port) {
